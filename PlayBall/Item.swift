669bc464427ca2a0f21dbe6b1017398d104bfd1b
Corey Gagnon <gagnon.corey@gmail.com>
2025-04-16 08:12:37 -0400

    Initial Commit

---
+++ PlayBall/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  PlayBall
+//
+//  Created by Corey Gagnon on 4/16/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ PlayBallTests/PlayBallTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlayBallTests.swift
+//  PlayBallTests
+//
+//  Created by Corey Gagnon on 4/16/25.
+//
+
+import Testing
+@testable import PlayBall
+
+struct PlayBallTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
